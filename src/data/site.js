6c262498207a1acb5a646f45ac9ef83a206fbf2a
James Thomason, Jr <james@jamesthomasonjr.com>
2020-05-12 18:07:08 -0400

    Update site.js

--- src/data/site.js
+++ src/data/site.js
@@ -1,24 +1,15 @@
-module.exports = function() {
-  return {
-    // Site settings
-    title: 'amandat.art',
-    email: 'amanda@thomason.email',
-    description: 'Work in progress',
-
-    // Logo settings
-    'logo-image-url': '/assets/images/logo.svg',
-    'font-awesome-icon-logo': null,
-
-    // Social accounts
-    github_username: null,
-    twitter_username: null,
-    facebook_url: null,
-    instagram_username: 'amandat.art',
-    dribble_username: null,
-    '500px_username': null,
-    phone: null,
-
-    // Layout settings
-    'home-link-in-menu': true,
-  };
+{
+  "title": "amandat.art",
+  "email": "amanda@thomason.email",
+  "description": "Work in progress",
+  "logo-image-url": "/assets/images/logo.svg",
+  "font-awesome-icon-logo": null,
+  "github_username": null,
+  "twitter_username": null,
+  "facebook_url": null,
+  "instagram_username": "amandat.art",
+  "dribble_username": null,
+  "500px_username": null,
+  "phone": null,
+  "home-link-in-menu": true
 }
